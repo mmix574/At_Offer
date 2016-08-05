029ab0f0b98cf697ec668b2617fe77dead8574a1
taita2015 <mmix.xia@gamil.com>
2016-08-05 21:57:36 +0800

    initial commit

--- 008.cpp
+++ 008.cpp
@@ -2,6 +2,40 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int pow(int a,int n){
+        //base
+        if(n==0){
+            return 1;
+        }else if(n==1){
+            return a;
+        }
+        else{
+            int a_half = pow(a,n/2);
+            if(n%2==0){
+                return a_half*a_half;
+            }else{
+                return a_half*a_half*a;
+            }
+        }
+    }
+    int jumpFloor(int number) {
+        int sum = 0;
+//        base
+        if(number==0){return 0;}
+        else if(number==1){return 1;}
+        else if(number==2){return 2;}
+        else if(number==3){return 3;}
+        else{
+            return jumpFloor(number-1)+jumpFloor(number-2);
+        }
+    }
+};
+
 int main(){
+    Solution s;
+   cout<< s.jumpFloor(4)<<endl;
+
     return 0;
 }
