46755f9c8a4c2a4756706c36179170f882b56a45
taita2015 <mmix.xia@gamil.com>
2016-08-08 13:21:55 +0800

    initial commit

--- 019.cpp
+++ 019.cpp
@@ -32,13 +32,45 @@ class Solution {
 public:
     vector<int> printMatrix(vector<vector<int> > matrix) {
 		vector<int> r_s;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int loop_count = ((row<col?row:col)+1)/2;
+
+        if(row==1){
+            return matrix[0];
+        }else if(col==1){
+            for (int i = 0; i < row; ++i) {
+                r_s.push_back(matrix[i][0]);
+            }
+            return r_s;
+        }
+        cout<<loop_count<<endl;
+        for (int loop = 0; loop < loop_count; ++loop) {
+            for (int i = loop; i < col-loop-1; ++i) {
+                r_s.push_back(matrix[loop][i]);
+//                cout<<matrix[loop][i]<<" ";
+            }
+            for (int i = loop; i < row-loop-1; ++i) {
+                r_s.push_back(matrix[i][row-loop-1]);
+//                cout<<matrix[i][row-loop-1]<<" ";
+            }
+            for (int i = col-1-loop; i >loop; i--) {
+                r_s.push_back(matrix[row-1-loop][i]);
+//                cout<<matrix[row-1-loop][i]<<" ";
+            }
+            for (int i = row-1-loop; i > loop; i--) {
+//                cout<<matrix[i][loop]<<" ";
+                r_s.push_back(matrix[i][loop]);
+            }
+        }
+
 		return r_s;
     }
 };
 
 
 #define ROW_NUM 5
-#define COL_NUM 3
+#define COL_NUM 5
 
 
 int main(){
