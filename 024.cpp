bb2e6ac7da8f38f64a3164f7eb5a4965608e0c16
taita2015 <512801539@qq.com>
2016-08-13 17:26:14 +0800

    恩、哟、

--- 024.cpp
+++ 024.cpp
@@ -17,17 +17,29 @@ struct TreeNode {
 class Solution {
 public:
 
+	vector<vector<int> > result;
 
-    bool dfs(TreeNode* root,int exp){
-
+    void dfs(TreeNode* root,int exp,vector<int > path){
+		//base
+		if(!root->left&&!root->right&&exp==root->val){
+			path.push_back(root->val);
+			result.push_back(path);
+		}else{
+			if(root->val<exp){
+				path.push_back(root->val);
+				if(root->left){
+					dfs(root->left,exp-root->val,path);
+				}
+				if(root->right){
+					dfs(root->left,exp-root->val,path);
+				}
+			}
+		}
     }
-
     vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
-        vector<vector<int>> r_s;
-
-
-
-        return r_s;
+		vector<int> t;
+		dfs(root,expectNumber,t);
+        return result;
     }
 };
 
