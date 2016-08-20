d9f6153ddefbdcc9d5d023205cae15dbaab6f142
taita2015 <512801539@qq.com>
2016-08-21 00:11:50 +0800

    talk is cheap

---
+++ 033.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int GetUglyNumber_Solution(int index) {
+        if(index<0) return 0;
+
+    }
+};
+
+int main(){
+    Solution s;
+
+    return 0;
+}
