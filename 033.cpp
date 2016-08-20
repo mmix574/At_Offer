594d475f9fd62273c914889a55997f36b4f500ef
taita2015 <512801539@qq.com>
2016-08-21 07:17:13 +0800

    talk is cheap

--- 033.cpp
+++ 033.cpp
@@ -7,12 +7,23 @@ public:
     int GetUglyNumber_Solution(int index) {
         if(index<0) return 0;
 //      5 4 3 2 5 4 3 2
-        
+        int d[4]={2,3,4,5};
+        int s = 1;
+        int i = 0;
+        while(index>>i){
+            if((index>>i)&1){
+                s=s*d[i%4];
+                printf("%d\n",i);
+            }
+            i++;
+        }
+        return s;
     }
 };
 
 int main(){
     Solution s;
+    cout<<s.GetUglyNumber_Solution(3)<<endl;
 
     return 0;
 }
