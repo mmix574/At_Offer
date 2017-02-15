99653631dbaed791f4ad6afc64946df445a6c28d
taita2015 <mmix.xia@gmail.com>
2017-02-15 17:38:16 +0800

    0215

--- 046.cpp
+++ 046.cpp
@@ -2,14 +2,63 @@
 // Created by taita on 2017/2/12.
 //
 #include <iostream>
-
 using namespace std;
 class Solution {
 public:
+    bool isNumChar(char c){
+        if('0'<=c&&'9'>=c){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    int charSign(char c){
+        if(c=='-'){
+            return -1;
+        }else if(c=='+'){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    bool validata(string s){
+        for (int i = 0; i < s.length(); ++i) {
+            if(!isNumChar(s[i])){
+                return false;
+            }
+        }
+        return true;
+    }
     int StrToInt(string str) {
+        int sign = 0;
+        str.erase(0,str.find_first_not_of(' '));
+        str.erase(str.find_last_not_of(' ')+1,str.length());
 
+        if(sign = charSign(str[0])){
+            str.erase(0);
+        }
+
+        cout<<str<<endl;
+        
+        if(!validata(str)){
+            return 0;
+        }
+
+        int r = 0;
+        for (int i = 0; i < str.length(); ++i) {
+            r*=10;
+            r+=(str[i])+(0-'0');
+        }
+        if(sign==-1){
+            return -r;
+        }else{
+            return r;
+        }
     }
 };
 int main(){
+    Solution s;
+    int a = s.StrToInt("  123  ");
+    cout<<a<<endl;
     return 0;
 }
