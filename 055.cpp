71a785abf38961a013ca5ca781e2ddbcebb3a464
taita2015 <mmix.xia@gmail.com>
2017-02-20 15:38:22 +0800

    0220

--- 055.cpp
+++ 055.cpp
@@ -17,6 +17,25 @@ class Solution {
 public:
     bool isSymmetrical(TreeNode* pRoot)
     {
+        if(!pRoot){ return NULL; }
+        return isMirror(pRoot->left,pRoot->right);
+    }
+    bool isMirror(TreeNode *A,TreeNode *B){
+        if(!A&&!B){
+            return true;
+        }else if((!A&&B)||(A&&!B)){
+            return false;
+        }else{
+            if(A->val!=B->val){
+                return false;
+            }else{
+                if(isMirror(A->left,B->right)&&isMirror(A->right,B->left)){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
 
     }
 };
