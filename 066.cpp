1a9ddcff8bb7bb21617b2dd81e62f1ad5917ece4
taita2015 <mmix.xia@gmail.com>
2017-03-11 11:00:11 +0800

    0311

---
+++ 066.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxInWindows(const vector<int>& num, unsigned int size)
+    {
+        vector<int> res(num);
+        int length = num.size();
+        for (int i = 0; i <= length-size; ++i)
+        {
+        	for (int j = i; j < i+size; j++)
+        	{
+        		for(int k = i;k < i+size; k++){
+        			if(res[k]<num[j]){
+        				res[k] = num[j];
+        			}
+        		}
+        	}
+        }
+        return res;
+    }
+};
+
+int main(){
+	Solution s;
+	vector<int> data_set = {2,3,4,2,6,2,5,1};
+	vector<int> res = s.maxInWindows(data_set,3);
+	for (int i = 0; i < res.size(); ++i)
+	{
+		cout<<res[i];
+		cout<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
