729ca575fa4ad1495b1edd8d4c8f9ba42c7ff938
taita2015 <mmix.xia@gmail.com>
2017-03-11 23:04:14 +0800

    0311

--- 043_fixme.cpp
+++ 043_fixme.cpp
@@ -1,34 +1,63 @@
-//
-// Created by taita on 2017/2/11.
-//
-
 #include <iostream>
 #include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
+
+typedef struct ListNode{
+	int val;
+	ListNode *next;
+}ListNode;
+
+void print(ListNode *p){
+	if(!p) return;
+	printf("%d ",p->val);
+	ListNode *t = p->next;
+	while(t&&t!=p){
+		printf("%d ",t->val);
+		t=t->next;
+	}
+}
 
 class Solution {
 public:
-    int getLive(int student[],n){
-        for (int i = 0; i < n; ++i)
-        {
-            if(student[i]==0){
-                return i;
-            }
-        }
-        return -1;
-    }
     int LastRemaining_Solution(int n, int m)
     {
-        if(!n) return 0;
-        bool student[n] = {0};
-        int dead = 0;
-        if(dead+1=n){
-            return getLive(student,n);
-        }
+    	ListNode *head = NULL;
+    	ListNode *tail = NULL;
+    	if(n<=0) return -1;
+    	if(n==1) return n;
+		for (int i = 0; i < n; ++i)
+    	{
+    		if(head==NULL){
+    			head=tail=(ListNode *)malloc(sizeof(ListNode));
+    			head->val=i;
+    			head->next = NULL;
+    		}else{
+    			tail->next = (ListNode *)malloc(sizeof(ListNode));
+    			tail = tail->next;
+    			tail->next = NULL;
+    			tail->val = i;
+    		}
+		} 
+		tail->next = head;
+		ListNode *t = tail;
+		while(n>1){
+			int cut = m%n;
+			for (int i = 0; i < cut; ++i)
+			{
+				t=t->next;
+			}
+			ListNode *p = t->next;
+			t->next = p->next;
+			cout<<p->val<<endl;
+			n--;
+		}
+		return t->val;
     }
 };
 
 int main(){
     Solution s;
-    cout<<s.LastRemaining_Solution(100,7)<<endl;
+    cout<<s.LastRemaining_Solution(5,3)<<endl;
 }
