0c7387e268be827d801e942565b7d4e7a33beb7c
taita2015 <mmix.xia@gmail.com>
2017-03-11 23:33:39 +0800

    0311

---
+++ 069_lasting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool hasPath(char* matrix, int rows, int cols, char* str)
+    {
+    
+    }
+};
+int main(){
+	return 0;
+}
